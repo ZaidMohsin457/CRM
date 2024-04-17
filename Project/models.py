1687adb722cc4ddf794bb4058ed5d13a32d8e3d0
Ali Faisal <147637957+alifaisal676@users.noreply.github.com>
2024-04-17 16:58:31 +0500

    Update models.py

--- Project/models.py
+++ Project/models.py
@@ -1,3 +1,37 @@
-from django.db import models
+from django.db import connection
 
 # Create your models here.
+def create_table():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                CREATE TABLE if not exists my_integer_data (
+                id SERIAL PRIMARY KEY,
+                value1 INT,
+                value2 INT,
+                value3 INT
+                );
+        """)
+def insert_data():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT INTO my_integer_data (value1, value2, value3)
+            VALUES
+            (10, 20, 30),
+            (15, 25, 35),
+            (8, 18, 28),
+            (12, 22, 32),
+            (5, 15, 25),
+            (30, 40, 50),
+            (7, 17, 27),
+            (22, 32, 42),
+            (14, 24, 34),
+            (18, 28, 38);
+        """)
+
+def retreive_data():
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT * FROM my_integer_data;
+        """)
+        data = cursor.fetchall()
+    return data
