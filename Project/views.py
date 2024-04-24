7e53de3cadce54e7fd2d6896083642927b922dd6
“ZaidMohsin457” <zaidmohsin45@gmail.com>
2024-04-24 23:29:06 +0500

    Merge branch 'main' of https://github.com/ZaidMohsin457/CRM

--- Project/views.py
+++ Project/views.py
@@ -63,3 +63,4 @@ def project_details(request):
     return render(request,'projects-view-details.html')
 def leads_pipeline(request):
     return render(request,'leads-pipeline.html')
+# hello i am ziaudiosf
