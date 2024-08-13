b6329f32b77464ac166b26850ba939f569f58a22
Muhammad Zaid Mohsin <147637998+ZaidMohsin457@users.noreply.github.com>
2024-08-13 21:39:27 +0500

    new

--- CS232P/settings.py
+++ CS232P/settings.py
@@ -131,3 +131,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+
+AWS_ACCESS_KEY_ID = 'AKIAZI2LE2K5A4IKTOR4'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'crmprojectbucket'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'Europe (Stockholm) eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
